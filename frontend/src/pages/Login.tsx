3caf2009cf3fdc1f0923e63cf8c61c65e0660ad2
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-06-24 11:56:19 +0530

    new update

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -14,9 +14,53 @@ function Login() {
 
 
         try{
-            const res = axios.post("")
+            const res = axios.post("http://localhost:3000/api/auth/login",{
+                name,
+                email,
+                password
+            })
+
+            console.log(res.data);
+            alert(res.data.message);
+            navigate("/login");
+
+
+        } catch(error){
+            console.log(error.res.data);
+            alert(error.res.data.message);
         }
 
     }
+
+    return (
+        <>
+        <div className="auth-page-wrapper">
+            <div className="auth-card">
+                <h1>create account </h1>
+
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label>name</label>
+                        <input type="text" name="name" required/>
+                    </div>
+                    <div>
+                        <label>email</label>
+
+                        <input type="email" name="email" required/>
+                    </div>
+                    <div>
+                        <label>password</label>
+                        <input type="password" name="password" required/>
+                    </div>
+                    <button type="submit">
+                        Login
+                    </button>
+                </form>
+            </div>
+
+        </div>
+        
+        </>
+    )
 
 }
