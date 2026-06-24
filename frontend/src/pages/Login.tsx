330795c07e8599b6eec315e313119ca5303400c0
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-06-24 12:06:08 +0530

    creating login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,8 @@
-
-
+import {useState} from "react" 
+import { useNavigate } from "react-router-dom";
 function Login(){
+    const {loading ,setloading} = useState(false);
+    const navigate = useNavigate();
     return (
         <>
         <h1>Login </h1>
