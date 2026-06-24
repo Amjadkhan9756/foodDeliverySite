564651d7c1606521b9e2afc8f9960e12da682f2d
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-06-24 11:58:12 +0530

    new update

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,66 +1,11 @@
 
-import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
-function Login() {
 
-    const navigate = useNavigate();
-
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-
-        const name = e.target.name.value;
-        const email = e.target.emil.value;
-        const password = e.target.password.value;
-
-
-        try{
-            const res = axios.post("http://localhost:3000/api/auth/login",{
-                name,
-                email,
-                password
-            })
-
-            console.log(res.data);
-            alert(res.data.message);
-            navigate("/login");
-
-
-        } catch(error){
-            console.log(error.res.data);
-            alert(error.res.data.message);
-        }
-
-    }
-
+function Login(){
     return (
         <>
-        <div className="auth-page-wrapper">
-            <div className="auth-card">
-                <h1>create account </h1>
-
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label>name</label>
-                        <input type="text" name="name" required/>
-                    </div>
-                    <div>
-                        <label>email</label>
-
-                        <input type="email" name="email" required/>
-                    </div>
-                    <div>
-                        <label>password</label>
-                        <input type="password" name="password" required/>
-                    </div>
-                    <button type="submit">
-                        Login
-                    </button>
-                </form>
-            </div>
-
-        </div>
-        
+        <h1>Login </h1>
         </>
     )
-
 }
+
+export default Login;
