b9a12d31df92db5cebb6c9d8f4f9f79b4b49cd8d
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-06-25 10:52:08 +0530

    creating login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,19 +1,30 @@
 import {useState} from "react" 
 import { useNavigate } from "react-router-dom";
 import axios from "axios"
+import toast from "react-hot-toast";
 
 
 
 function Login(){
-    const {loading ,setloading} = useState(false);
+    const {loading ,setLoading} = useState(false);
     const navigate = useNavigate();
 
     const responseGoogle = async(authResult:any) =>{
         setLoading(true);
         try{
-            const result = await axios.post()
+            const result = await axios.post(`$(authService)/api/auth/login`,{
+                code:authResult["code"];
+
+            });
+            localStorage.setItem("token", result.data.token);  
+            toast.success(result.data.message);
+            navigate("/");
+
 
         } catch(error){
+            console.log(error);
+            toast.error("Problem occurred while logging in")
+            setLoading(false);
            
         }
     }
