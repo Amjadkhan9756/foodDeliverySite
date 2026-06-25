90c2112a089818f5db08c780d1d0dbe0cfeda961
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-06-25 10:58:28 +0530

    creating upload file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+
+export const authService = "http://localhost:3000";   
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="915359388751-69df2m12lgbatldedqvsor4e84fhm5mp.apps.googleusercontent.com"> <App /></GoogleOAuthProvider>;
+
+   
   </StrictMode>,
 )
