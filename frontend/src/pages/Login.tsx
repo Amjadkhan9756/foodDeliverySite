f39b95867021f3bca9647ec4a288011414d8c76e
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-06-25 10:34:46 +0530

    creating login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,22 @@
 import {useState} from "react" 
 import { useNavigate } from "react-router-dom";
+import axios from "axios"
+
+
+
 function Login(){
     const {loading ,setloading} = useState(false);
     const navigate = useNavigate();
+
+    const responseGoogle = async(authResult:any) =>{
+        setLoading(true);
+        try{
+            const result = await axios.post()
+
+        } catch(error){
+           
+        }
+    }
     return (
         <>
         <h1>Login </h1>
