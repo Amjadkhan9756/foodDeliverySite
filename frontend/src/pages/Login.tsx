b1df0c753210148056760c42cb696716c65bf37b
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-06-28 10:25:03 +0530

    new update

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -46,7 +46,21 @@ function Login() {
     })
     return (
         <>
-           
+            <div className="flex min-h-screen items-center justify-center bg-white px-4">
+                <div className="w-full max-w-sm space-y-6 ">
+                    <h1 className="text-center text-3xl font-bold text-[#E23774]">foodDeliverySite</h1>
+
+                    <p className="text-center text-sm text-gray-500">Login or signup to continue</p>
+                    <button onClick={googleLogin} disabled={loading} className="flex w-full items-center justify-center gap-3 rounded-xl border border-gey-300 bg-white px-4 py-3 ">
+                        <FcGoogle size={20} />{loading ? "Signing in..." : "Continue with Google"}
+                    </button>
+                </div>
+
+                <p className="text-center text-xs text-grey-400 ">By continuing you agree our {""}
+                    <span className="text-[#E23774]">Terms of Service</span> ${" "} 
+                    <span className="text-[#E23774]">Privacy Policy</span>
+                </p>
+            </div>
         </>
     )
 }
