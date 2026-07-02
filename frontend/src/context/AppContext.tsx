782c5454e3acfb868ec2fb33c7d35f1447c056ab
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-07-02 17:14:14 +0530

    creating AppContext

--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, useState, type ReactNode } from "react"
-
-
+import { createContext, use, useState, type ReactNode } from "react"
+import { useEffect } from "react";
+import axios from "axios";
 
 
 const AppContext  =  createContext(undefined)
@@ -14,7 +14,35 @@ interface AppProviderProps {
 export const AppProvider = ({children}:AppProviderProps) => {
     const [user,setUser] = useState(null);
     const [isAuth,setIsAuth] = useState(false);
-    const [loading,setLoading] = useState(false);
+    const [loading,setLoading] = useState(true);
 
+    const [location ,serLocation] = useState(null);
+    const [loadingLocation ,setLoadingLocation ] = useState(false);
+
+    const [city,setCity] = useState("fecting Location...");
+    async function fetchUser(){
+        try{
+            const token = localStorage.getItem("token");
+            const  {data} = await axios.get(`$(authService)/api/auth/me`,{
+                headers :{
+                 Authorization:`Bearer ${token}`,
+                },
+
+            });
+            setUser(data.user);
+            setIsAuth(true);
+
+        } catch(error){
+            console.log(error);
+
+        } finally{
+            setLoading(false);  
+        }
+    }
+
+
+    useEffect(()=>{
+        fetchUser();
+    },[])
 }
     
