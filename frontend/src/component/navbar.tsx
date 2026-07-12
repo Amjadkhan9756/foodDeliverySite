e6e868965d29b65bd37375767b4592817e128a75
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-07-12 12:15:18 +0530

    creating navbar component

--- frontend/src/component/navbar.tsx
+++ frontend/src/component/navbar.tsx
@@ -1,8 +1,8 @@
 import { useLocation,useSearchParams } from "react-router-dom";
 import { useAppData } from "../context/AppContext";
 
-import {  useState } from "react";
-
+import {   useEffect, useState } from "react";
+    
 
 const Navbar = ()=>{
     const {isAuth } = useAppData();
@@ -11,6 +11,19 @@ const Navbar = ()=>{
     const {searchParams,setSearchParams} = useSearchParams();
     const {search,setSearch} = useState(searchParams.get("search") || "");
 
+
+    useEffect(()=>{
+        const timer =   setTimeout(()=>{
+            if(search){
+                setSearchParams({search})
+            }
+            else{
+                setSearchParams({ })
+            }
+            return ()=>clearTimeout(timer)
+        }, 500)
+
+    }, [search])
     return (
         <>
         <h1>Navbar</h1>
